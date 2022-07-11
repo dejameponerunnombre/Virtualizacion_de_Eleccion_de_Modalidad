ec640cc901a341951161fc6ac94b20bf46e68662
Patricio Altamiranda <68668425+dejameponerunnombre@users.noreply.github.com>
2022-07-11 16:40:36 -0300

    ahora si anda je

--- PROYECTO/promedios2.0.php
+++ PROYECTO/promedios2.0.php
@@ -1,34 +1,30 @@
 <?php
 include("db.php");
 for($x = 1; $x <= 5; $x++)
-{    
-    $pepe = 0;
-    $ahorasi="SELECT DNI FROM eleccion where ID_Modalidad = $x";
+{    //http://localhost/PROYECTO/promedios2.0.php
+    $ahorasi="SELECT Ingresos FROM modalidad where ID_Modalidad = $x";
     $quesi= $conexion->query($ahorasi);
     $siquesi = $quesi ->fetch_array();
-    for($i = 1; $i <= 10; $i++)
+    $i = 0; //update eleccion set Prioridad = 0 
+    for($y = 1; $y <= $siquesi[0]; $y++)
     {   
-        $varB = "SELECT DNI FROM total  where DNI != $pepe order by total.PromediosT DESC";
+        $varB = "SELECT t.DNI FROM total t , eleccion e where t.DNI = e.DNI 
+        and e.ID_Modalidad = $x and e.Prioridad = 0 
+        order by t.PromediosT DESC";
         $connB = $conexion -> query($varB);
         $DNI = $connB ->fetch_array();
-        $pepe = $DNI[0];
-        $var = "SELECT PromediosT FROM total where DNI = $DNI[0] order by total.PromediosT DESC";
-        $conn = $conexion -> query($var);
-        $Prom = $conn ->fetch_array();
-        $var2 = "UPDATE eleccion SET Prioridad = $i where DNI = $DNI[0] and ID_Modalidad = $x";
+        $var2 = "UPDATE eleccion SET Prioridad = $y where DNI = $DNI[$i] and ID_Modalidad = $x";
         $conn2= $conexion->query($var2);
-        echo "a";
-        if($i < 39)
+        if($y < 39)
         {    
-            $var3 = "UPDATE eleccion SET Situacion = 'Dentro del curso' where DNI = $DNI[0]";
+            $var3 = "UPDATE eleccion SET Situacion = 'Dentro del curso' where DNI = $DNI[$i]";
             $conn3 = $conexion->query($var3);
         }
         else {    
-            $var4 = "UPDATE eleccion SET Situacion = 'En lista de espera' where DNI = $DNI[0]";
+            $var4 = "UPDATE eleccion SET Situacion = 'En lista de espera' where DNI = $DNI[$i]";
             $conn4 = $conexion->query($var4);
-        }
+            }
     }
+    $i++;
 }
-
-
 ?>
