95537c8f38ae10b26147a4355638e6e24394eab7
melitroncoso <68672246+melitroncoso@users.noreply.github.com>
2022-08-08 01:27:22 -0300

    intento de diseño de tablas

--- PROYECTO/promedios2.0.php
+++ PROYECTO/promedios2.0.php
@@ -1,4 +1,88 @@
-<?php
+<!DOCTYPE html>
+<html lang="es" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Listas de Modalidades</title>
+
+    <link href="bootstrap.css" rel="stylesheet" />
+    <link href="font-awesome.css" rel="stylesheet" />
+    <link href="custom.css" rel="stylesheet" />
+    <link href="tablas.css" rel="stylesheet" />
+    <link rel="stylesheet" href="footer.css" >
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+     
+           
+          
+    <div id="wrapper">
+         <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="adjust-nav">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="logo" href="#" ><img src="../IMG/Escuela.png" style="width: 40px; margin-top: 2px;"/></a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Cerrar Sesión</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    <li class="text-center user-image-back">
+                        <img src="../IMG/find_user.png" class="img-responsive" />
+                     
+                    </li>
+
+
+                    <li>
+                        <a href="form-ingreso.html"><i class="fa fa-desktop "></i>Cargar notas</a>
+                    </li>
+                   
+
+                    <li>
+                        <a href="promedios2.0.php"><i class="fa fa-table "></i>Ver listas de cada modalidad</a>
+                    </li>
+                    
+                    <li>
+                        <a href="#"><i class="fa fa-trash-o"></i>Eliminar alumno</a>
+                    </li>
+                    <li>
+                        <a href="form_ingreso_DNI.html"><i class="fa fa-bar-chart-o"></i>Cambiar notas</a>
+                    </li>
+
+                    <li>
+                        <a href="cambio_eleccion.html"><i class="fa fa-edit "></i>Permitir rehacer elección</a>
+                    </li>
+                    
+                </ul>
+
+            </div>
+
+        </nav>
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    
+                    <div class="col-md-12">
+                     <h2>Listas por modalidad</h2>   
+                    </div>
+                </div>              
+               
+                  <hr />
+              
+                  <?php
 $inicio = mktime(0, 0, 0, 5, 1, 2022);
 $final =  mktime(0, 0, 0, 10, 15, 2022);
 $ahora = time() ;
@@ -16,7 +100,7 @@ for($x = 1; $x <= 5; $x++)
     $siquesi = $quesi ->fetch_array();
     ?>
     <h1> <?php echo $siquesi["Descripcion"] ?></h1>
-        <table border = 1><tr><th>Puesto</th><th>Alumno</th><th>Promedio</th><th>Fichas</th><th>Observaciones</th><th>Inasistencias</th><th>Comentario</th></tr>
+     <div class="datagrid">   <table border = 1><tr><th>Puesto</th><th>Alumno</th><th>Promedio</th><th>Fichas</th><th>Observaciones</th><th>Inasistencias</th><th>Comentario</th></tr>
     <?php
     $i = 0; 
     for($y = 1; $y <= 39 && $y <= $siquesi[0]; $y++)
@@ -49,9 +133,9 @@ for($x = 1; $x <= 5; $x++)
     }
     $i++;
     ?>
-    </table>
+    </table></div>
     <h1>Tabla de espera de <?php echo $siquesi["Descripcion"]?></h1>
-    <table border = 1><tr><th>Puesto</th><th>Alumno</th><th>Promedio</th><th>Fichas</th><th>Observaciones</th><th>Inasistencias</th><th>Comentario</th></tr>
+    <div class="datagrid"><table border = 1><tr><th>Puesto</th><th>Alumno</th><th>Promedio</th><th>Fichas</th><th>Observaciones</th><th>Inasistencias</th><th>Comentario</th></tr>
     <?php
     for($y = 40; $y <= $siquesi[0]; $y++)
     {   
@@ -69,7 +153,7 @@ for($x = 1; $x <= 5; $x++)
         } 
     }
     ?>
-    </table>
+    </table></div>
     <?php 
 }
 }
@@ -78,3 +162,25 @@ else
     include("mensaje-error.html");
 }
 ?>  
+                 <!-- /. ROW  -->           
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+   
+        </div>
+        </tbody>
+        <script src="../js/jquery-1.10.2.js"></script>
+        <!-- BOOTSTRAP SCRIPTS -->
+      <script src="../js/bootstrap.min.js"></script>
+      <!-- METISMENU SCRIPTS -->
+      <script src="../js/jquery.metisMenu.js"></script>
+        <!-- CUSTOM SCRIPTS -->
+      <script src="../js/custom.js"></script>
+   
+
+<footer class="pie-pagina" style="left: 0; right: 0;">
+    <div class="grupo-2">
+        <small>&copy; 2022 <b>7mo Informática</b> - Grupo 3</small>
+    </div>
+</footer>
+</html>
