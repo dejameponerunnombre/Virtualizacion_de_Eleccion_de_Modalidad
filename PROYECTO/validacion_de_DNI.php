37c339702a7b889965cb37b7631c66941a9d7ea8
AlbeJuanCruz <101598251+AlbeJuanCruz@users.noreply.github.com>
2022-08-11 16:44:16 -0300

    Diesño de página de cambio de notas, a casi terminar
    Lo subo por si se rompe todo otra vez

--- PROYECTO/validacion_de_DNI.php
+++ PROYECTO/validacion_de_DNI.php
@@ -5,8 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
+    <link rel="stylesheet" href="footer.css">
+    <link href="bootstrap.css" rel="stylesheet" />
+  
+  <link href="font-awesome.css" rel="stylesheet" />
+
+  <link href="custom.css" rel="stylesheet" />
     
-    <link rel="stylesheet" href="style_ingreso.css">
 
     <title>Control de notas</title>
 </head>
@@ -36,8 +41,65 @@ if($filas > 0){
     $_SESSION['filas2'] = $fila2;
     $_SESSION['filas3'] = $fila3;
     ?>
-    <div class="m_formulario">
-        <div style="padding:20px;">
+    
+    <div id="wrapper">
+         <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="adjust-nav">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="logo" href="#" ><img src="../IMG/Escuela.png" style="width: 40px; margin-top: 2px;"/></a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Cerrar Sesión</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    <li class="text-center user-image-back">
+                        <img src="../IMG/find_user.png" class="img-responsive" />
+                    </li>
+                    <li>
+                        <a id= 1 href="form-ingreso.php"><i class="fa fa-desktop "></i>Cargar notas</a>
+                    </li>
+                    <li>
+                        <a href="promedios2.0.php"><i class="fa fa-table "></i>Ver listas de cada modalidad</a>
+                    </li>
+                    <li>
+                        <a href="eliminar-alumno.php"><i class="fa fa-trash-o "></i>Eliminar alumno</a>
+                    </li>
+                    <li>
+                        <a href="form_ingreso_DNI.php"><i class="fa fa-bar-chart-o"></i>Cambiar notas</a>
+                    </li>
+                    <li>
+                        <a href="cambio-eleccion.php"><i class="fa fa-edit "></i>Permitir rehacer elección</a>
+                    </li>
+                    
+                </ul>
+
+            </div>
+
+        </nav>
+       
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>Seleccione una opción</h2>   
+                    </div>
+                </div>              
+               
+                  <hr />  
+                  <div class="m_formulario">
+        <div>
                 <h2>Ingresar datos de alumno</h2>
             <br>
             
@@ -95,7 +157,31 @@ if($filas > 0){
             </ul>
             </div>
         </div>
-        </form>
+        </form>         
+    </div>
+            </div>
+   
+        </div>
+ 
+    <script src="../js/jquery-1.10.2.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.metisMenu.js"></script>
+    <script src="../js/custom.js"></script>
+    
+   
+
+    
+
+
+
+
+    <footer class="pie-pagina" style="left: 0;" style="right: 0;">
+        <div class="grupo-2">
+            <small>&copy; 2022 <b>7mo Informática</b> - Grupo 3</small>
+        </div>
+    </footer>
+
+
         <?php   
 }else{
     ?>
