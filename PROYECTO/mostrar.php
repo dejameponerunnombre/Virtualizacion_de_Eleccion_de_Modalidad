f00a43d54df679ce893a761306d38cb6a9edd9ca
AlbeJuanCruz <albejuancruz@gmail.com>
2022-09-26 17:14:08 -0300

    Cambios generales

--- PROYECTO/mostrar.php
+++ PROYECTO/mostrar.php
@@ -26,9 +26,11 @@ if($ahora < $final && $ahora > $inicio)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../IMG/Logo_azul (2).PNG">
-    <link rel="stylesheet" href="inicio.css">
+    <link rel="stylesheet" href="eleccion.css">
+
     <link rel="stylesheet" href="footer1.css ?v=<?php echo time(); ?>">
     <script src="https://kit.fontawesome.com/2eed8f0a03.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="footer.css">
 
     <title>Resultado de elección</title>
   </head>
@@ -40,7 +42,7 @@ if($ahora < $final && $ahora > $inicio)
         <nav>
             <div>
 
-                <a href="inicio.html"><img src="../IMG/Escuela.png" alt="Imagen de fondo" class="logo"></a>
+                <a href="inicio.html"><img src="../IMG/logo (3).png" alt="Imagen de fondo" class="logo"></a>
 
             </div>
             <input type="checkbox" name="check" id="check">
@@ -59,9 +61,9 @@ if($ahora < $final && $ahora > $inicio)
         <div class="texto-centro">
            <div class="mesaje_verde" style="color:green;">
 
-            <h1>El alumno <?php echo $alumnos[0] ?> esta <?php echo $eleccion[0] ?> de <?php echo $modalidad[0]?> en el puesto <?php echo $eleccion[1]?></h1>
-
-            <p>Si tiene alguna consulta o inquietud, porfavor comunicarse con secretaría.</p>
+            <!--<h1>El alumno <?php echo $alumnos[0] ?> esta <?php echo $eleccion[0] ?> de <?php echo $modalidad[0]?> en el puesto <?php echo $eleccion[1]?></h1>-->
+            <h1>Estás <?php echo $eleccion[0] ?> de <?php echo $modalidad[0]?></h1>
+            <p>Si tiene alguna consulta o inquietud, porfavor comunicarse con <a href="mailto:secretaria@gmail.com">Secretaría</a></p>
 
             </div>
         </div>
@@ -82,7 +84,12 @@ if($ahora < $final && $ahora > $inicio)
 
 
 
-  
+    <footer class="pie-pagina">
+        
+        <div class="grupo-2">
+            <small>&copy; 2022 <b>7mo Informática</b> - Todos los Derechos Reservados.</small>
+        </div>
+    </footer>
     
   </body>
   </html>
