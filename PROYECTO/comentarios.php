1e21cf5f243e7757e711d7d3c46976672be37638
MatyBotta <68671511+MatyBotta@users.noreply.github.com>
2022-10-17 16:55:11 -0300

    Update comentarios.php

--- PROYECTO/comentarios.php
+++ PROYECTO/comentarios.php
@@ -1,11 +1,14 @@
 <?php
 
-$destino = "melinatroncosoleal@gmail.com";
+/*$destino = "dielmod7c@hotmail.com";
 $name = $_POST["name"];
 $email = $_POST["email"];
 $mensaje = $_POST["mensaje"];
 $contenido = "Nombre: " . $name . "\nCorreo: " . $email . "\nMensaje: " . $mensaje;
-mail($destino, $mensaje, $contenido);
-echo "<script>alert('Correo enviado exitosamente.')</script>";
-echo "<script> setTimeout(\"location.href='inicio.html'\",1000)</script>";
+if(mail($destino,"CONSULTA PAG", $mensaje, $contenido))
+{
+    echo "<script>alert('Correo enviado exitosamente.')</script>";
+}
+echo "<script> setTimeout(\"location.href='inicio.html'\",10000)</cript>";*/
+mail("dielmod7c@outlook.com","CONSULTA PAG", "soy el mensaje", "lalala");
 ?>
